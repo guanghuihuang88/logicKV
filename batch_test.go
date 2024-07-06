522a46a7e1536b465a2c5789face9666ec729ee8
logichuang <logichuang@tencent.com>
2024-07-06 16:32:54 +0800

    调通【事务】相关代码测试

--- batch_test.go
+++ batch_test.go
@@ -7,15 +7,16 @@ import (
 	"testing"
 )
 
-func TestDB_WriteBatch(t *testing.T) {
-	options := DefaultOptions
+func TestDB_WriteBatch1(t *testing.T) {
+	opts := DefaultOptions
 	dir, _ := os.MkdirTemp("", "bitcask-go-batch-1")
-	options.DirPath = dir
-	db, err := Open(options)
+	opts.DirPath = dir
+	db, err := Open(opts)
 	defer destroyDB(db)
 	assert.Nil(t, err)
 	assert.NotNil(t, db)
 
+	// 写数据之后并不提交
 	wb := db.NewWriteBatch(DefaultWriteBatchOptions)
 	err = wb.Put(utils.GetTestKey(1), utils.RandomValue(10))
 	assert.Nil(t, err)
@@ -23,13 +24,63 @@ func TestDB_WriteBatch(t *testing.T) {
 	assert.Nil(t, err)
 
 	_, err = db.Get(utils.GetTestKey(1))
-	assert.Equal(t, ErrKeyIsEmpty, err)
+	assert.Equal(t, ErrKeyNotFound, err)
+
+	// 正常提交数据
+	err = wb.Commit()
+	assert.Nil(t, err)
+
+	val1, err := db.Get(utils.GetTestKey(1))
+	assert.NotNil(t, val1)
+	assert.Nil(t, err)
+
+	// 删除有效的数据
+	wb2 := db.NewWriteBatch(DefaultWriteBatchOptions)
+	err = wb2.Delete(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	err = wb2.Commit()
+	assert.Nil(t, err)
+
+	_, err = db.Get(utils.GetTestKey(1))
+	assert.Equal(t, ErrKeyNotFound, err)
+}
+
+func TestDB_WriteBatch2(t *testing.T) {
+	opts := DefaultOptions
+	dir, _ := os.MkdirTemp("", "bitcask-go-batch-2")
+	opts.DirPath = dir
+	db, err := Open(opts)
+	defer destroyDB(db)
+	assert.Nil(t, err)
+	assert.NotNil(t, db)
+
+	err = db.Put(utils.GetTestKey(1), utils.RandomValue(10))
+	assert.Nil(t, err)
+
+	wb := db.NewWriteBatch(DefaultWriteBatchOptions)
+	err = wb.Put(utils.GetTestKey(2), utils.RandomValue(10))
+	assert.Nil(t, err)
+	err = wb.Delete(utils.GetTestKey(1))
+	assert.Nil(t, err)
 
 	err = wb.Commit()
 	assert.Nil(t, err)
 
-	val, err := db.Get(utils.GetTestKey(1))
-	assert.NotNil(t, val)
+	err = wb.Put(utils.GetTestKey(11), utils.RandomValue(10))
+	assert.Nil(t, err)
+	err = wb.Commit()
 	assert.Nil(t, err)
 
+	// 重启
+	err = db.Close()
+	assert.Nil(t, err)
+
+	db2, err := Open(opts)
+	assert.Nil(t, err)
+
+	_, err = db2.Get(utils.GetTestKey(1))
+	assert.Equal(t, ErrKeyNotFound, err)
+
+	// 校验序列号
+	assert.Equal(t, uint64(2), db.seqNo)
 }
